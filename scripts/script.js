486d7972183d59c19a570f6977b90127b89c4719
AleksGri <aleks032118@gmail.com>
2021-08-03 01:28:47 -0700

    FEAT(index.html): add if statement

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,55 @@
-let num = 266219;
-let mult = 1;
+// let num = 266219;
+// let mult = 1;
 
-while (num > 0) {
-  let digit = num % 10;
-  mult *= digit;
-  num = Math.floor(num / 10);
+// while (num > 0) {
+//   let digit = num % 10;
+//   mult *= digit;
+//   num = Math.floor(num / 10);
+// }
+
+// console.log(mult);
+
+// let power = mult ** 3;
+// console.log(power.toString().substring(0,2));
+
+//lesson03
+// 1)
+const lang = prompt('Укажите язык: "ru" или "en"', 'ru');
+if (lang === 'ru') console.log(
+  'Понедельник, вторик, среда, четверг, пятница, суббота, воскресенье');
+else if (lang === 'en') console.log(
+  'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday and Sunday');
+else {
+  console.log('Wrong input');
 }
 
-console.log(mult);
 
-let power = mult ** 3;
-console.log(power.toString().substring(0,2));
+switch (lang) {
+  case ('ru') : console.log(
+  'Понедельник, вторик, среда, четверг, пятница, суббота, воскресенье');
+  break;
+  case ('en') : console.log(
+  'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday and Sunday');
+  break;
+  default: console.log('Wrong input');
+}
+
+const days = [['ru', 'en'],
+  ['Понедельник, вторик, среда, четверг, пятница, суббота, воскресенье',
+  'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday and Sunday']
+];
+console.log(days[1][days[0].indexOf(lang)]);
+
+
+// 2)
+const namePerson = prompt('Укажите имя').toLowerCase();
+
+console.log(
+  (namePerson === 'артем') ? 'директор' :
+  (namePerson === 'максим') ? 'преподаватель' :
+                                                'студент');
+
+
+
+
+
