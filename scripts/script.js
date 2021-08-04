856dc11c1ae02046aa274c331a175143fdcac8c6
AleksGri <aleks032118@gmail.com>
2021-08-04 01:13:21 -0700

    FIX(index.html): refactor

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,27 @@
-// let num = 266219;
-// let mult = 1;
-
-// while (num > 0) {
-//   let digit = num % 10;
-//   mult *= digit;
-//   num = Math.floor(num / 10);
-// }
-
-// console.log(mult);
-
-// let power = mult ** 3;
-// console.log(power.toString().substring(0,2));
 
 //lesson03
 // 1)
+const days = [['ru', 'en'],
+  ['Понедельник, вторик, среда, четверг, пятница, суббота, воскресенье',
+  'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday and Sunday']
+];
+
 const lang = prompt('Укажите язык: "ru" или "en"', 'ru');
-if (lang === 'ru') console.log(
-  'Понедельник, вторик, среда, четверг, пятница, суббота, воскресенье');
-else if (lang === 'en') console.log(
-  'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday and Sunday');
+if (lang === 'ru') console.log(days[1][0]);
+else if (lang === 'en') console.log(days[1][1]);
 else {
   console.log('Wrong input');
 }
 
 
 switch (lang) {
-  case ('ru') : console.log(
-  'Понедельник, вторик, среда, четверг, пятница, суббота, воскресенье');
+  case ('ru') : console.log(days[1][0]);
   break;
-  case ('en') : console.log(
-  'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday and Sunday');
+  case ('en') : console.log(days[1][1]);
   break;
   default: console.log('Wrong input');
 }
 
-const days = [['ru', 'en'],
-  ['Понедельник, вторик, среда, четверг, пятница, суббота, воскресенье',
-  'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday and Sunday']
-];
 console.log(days[1][days[0].indexOf(lang)]);
 
 
