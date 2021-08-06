b1d9e8b644b6583c7a88ce4ee0428b90fa499910
AleksGri <aleks032118@gmail.com>
2021-08-06 01:26:06 -0700

    FEAT(script.js): add function

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,29 @@
+function cutter (newString) {
+  if (typeof newString !== 'string') return 'WRONG INPUT';
+  
+  while (newString[0] === ' ') {
+    console.log(newString[0]);
+    newString = newString.slice(1);
+  }
 
-//lesson03
-// 1)
-const days = [['ru', 'en'],
-  ['Понедельник, вторик, среда, четверг, пятница, суббота, воскресенье',
-  'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday and Sunday']
-];
+  while (newString.slice(-1) === ' ') {
+    newString = newString.slice(0, -1);
+  }
 
-const lang = prompt('Укажите язык: "ru" или "en"', 'ru');
-if (lang === 'ru') console.log(days[1][0]);
-else if (lang === 'en') console.log(days[1][1]);
-else {
-  console.log('Wrong input');
+  if (newString.length > 30) {
+    newString = newString.slice(0, 30) + '...';
+
+  }
+  
+  return newString;
 }
 
-
-switch (lang) {
-  case ('ru') : console.log(days[1][0]);
-  break;
-  case ('en') : console.log(days[1][1]);
-  break;
-  default: console.log('Wrong input');
-}
-
-console.log(days[1][days[0].indexOf(lang)]);
+console.log(cutter(6));
+console.log("_________________________________________")
+console.log(cutter('   паровозик, который смог   '));
+console.log(cutter('   123456789012345678901234567890|1234567890  '));
 
 
-// 2)
-const namePerson = prompt('Укажите имя').toLowerCase();
-
-console.log(
-  (namePerson === 'артем') ? 'директор' :
-  (namePerson === 'максим') ? 'преподаватель' :
-                                                'студент');
 
 
 
